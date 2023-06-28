83da4d5c31e4a9fe74adaeb8fabdc4bd041b810c
GayathriDevi01 <82216878+GayathriDevi01@users.noreply.github.com>
2023-06-28 09:56:54 +0530

    Create Armstrong Numbers.java

---
+++ Know Basic Math/Armstrong Numbers.java
@@ -0,0 +1,45 @@
+/*
+For a given 3 digit number, find whether it is armstrong number or not. An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. Return "Yes" if it is a armstrong number else return "No".
+NOTE: 371 is an Armstrong number since 33 + 73 + 13 = 371
+
+  */
+
+//CODE
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.armstrongNumber(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    static String armstrongNumber(int n){
+        // code here
+        int temp=n;
+        int sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=Math.pow(rem,3);
+            n/=10;
+        }
+        if(sum==temp)
+           return "Yes";
+        else
+           return "No";
+    }
+}
