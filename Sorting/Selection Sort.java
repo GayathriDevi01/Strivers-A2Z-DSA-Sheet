7c8e0c1535659d3e230e21ecae0a562f627c1d51
GayathriDevi01 <82216878+GayathriDevi01@users.noreply.github.com>
2023-07-01 15:51:43 +0530

    Create Selection Sort.java

---
+++ Sorting/Selection Sort.java
@@ -0,0 +1,59 @@
+//Traverse the entire array.With each iteration find the minimum value in the array and swap it with current element(i)
+ //{ Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        // code here such that selectionSort() sorts arr[]
+        selectionSort(arr,i);
+        return 0;
+	}
+	
+	void selectionSort(int arr[], int n)
+	{
+	    //code here
+	    for(int i=0;i<n-1;i++){
+	        int min=i;
+	        for(int j=i+1;j<n;j++){
+	            if(arr[min]>arr[j])
+	            min=j;
+	        }
+	    int temp=arr[min];
+	    arr[min]=arr[i];
+	    arr[i]=temp;
+	}
+	    
+	}
+} 
